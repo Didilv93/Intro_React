d5e0628ca2e9bf8692c9bafa387ddbacc28b7d36
Didilv93 <diegossufmg@gmail.com>
2018-10-17 10:28:28 -0300

    Brincando com maps

--- src/index/App.jsx
+++ src/index/App.jsx
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
-import Game from '../components/game/index';
+// import Game from '../components/game/index';
 import './styles/App.css';
 
 class App extends Component {
   render() {
+    const list = {
+      name : ['Nome 1', 'Nome 2', 'Nome 3', 'Nome 4', ],
+      idade : [10,20,30,40]
+    }
+    const teste = list.name.map((name, i) => <li>{'nome: ' + name + ' idade: '+ list.idade[i]}</li>);
     return (
       <React.Fragment>
-        <Game />
+        {/* <Game /> */}
+        {teste}
       </React.Fragment>
     );
   }
